af9fdb2376f00f927c3f6a6aebfd34997b95c297
CodedBeats <luca.haar@icloud.com>
2025-02-07 18:04:09 +1100

    initialised firebase for react native, setup config file for details to be added

---
+++ config/Config.ts
@@ -0,0 +1,9 @@
+// can just gitignore this file, or use .env stuff
+export const firebaseConfig = {
+    apiKey: "apikey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId"
+};
