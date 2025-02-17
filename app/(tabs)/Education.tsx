623e6fde1d5e882acdbd5c32dee88f7b88eac6b8
CodedBeats <luca.haar@icloud.com>
2025-02-18 00:13:12 +1100

    added basic frontend education page

---
+++ app/(tabs)/Education.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, View, Text, StyleSheet, Button } from 'react-native';
+import { useRouter } from "expo-router";
+
+// firebase
+import { collection, getDocs } from "firebase/firestore";
+
+// context
+import { useAuth } from "@/contexts/AuthContext";
+import { useDB } from "@/contexts/DBContext";
+
+
+export default function Education() {
+  const router = useRouter();
+  const { user, userEmail, logout } = useAuth()
+
+  // contexts
+  const db = useDB()
+
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+
+        {/* header placeholder */}
+        <View style={styles.headerPlaceholder}>
+          <Text>header component with search and profile</Text>
+        </View>
+
+        {/* education content */}
+        <View style={styles.container}>
+          {/* new articles */}
+          <View style={[styles.largeBox, styles.largeBox1]}>
+            <View style={styles.subBoxHeaderContainer}>
+              <Text style={styles.subBoxHeaderText}>New Articles</Text>
+            </View>
+            <View style={styles.subBoxContent}>
+              <Text>dynamically rendered new articles here</Text>
+            </View>
+          </View>
+
+          {/* article categories */}
+          <View style={[styles.largeBox, styles.largeBox2]}>
+            <View style={styles.subBoxHeaderContainer}>
+              <Text style={styles.subBoxHeaderText}>Browse by Category</Text>
+            </View>
+            <View style={styles.subBoxContent}>
+              <Text>dynamically rendered categories here</Text>
+            </View>
+          </View>
+
+          {/* trending articles */}
+          <View style={[styles.largeBox, styles.largeBox1]}>
+            <View style={styles.subBoxHeaderContainer}>
+              <Text style={styles.subBoxHeaderText}>Trending Articles</Text>
+            </View>
+            <View style={styles.subBoxContent}>
+              <Text>dynamically rendered trending articles here</Text>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+
+// styles
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  scrollView: {
+    flexGrow: 1,
+    paddingVertical: 20,
+  },
+  headerPlaceholder: {
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#888',
+    marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  largeBox: {
+    borderRadius: 10,
+    padding: 8,
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3, // android shadow
+  },
+  largeBox1: {
+    backgroundColor: '#520073',
+  },
+  largeBox2: {
+    backgroundColor: '#C56200',
+  },
+  subBoxHeaderContainer: {
+    marginBottom: 8,
+  },
+  subBoxHeaderText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#fff",
+    textDecorationLine: "underline",
+  },
+  subBoxContent: {
+    borderColor: "#222",
+    borderWidth: 1,
+    height: 100,
+  },
+});
