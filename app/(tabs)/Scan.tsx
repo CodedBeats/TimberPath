cc872f3e3098cdce02c19ae1b8100da247a51673
CodedBeats <luca.haar@icloud.com>
2025-02-18 01:22:35 +1100

    added basic frontend scan page

---
+++ app/(tabs)/Scan.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useRouter } from "expo-router";
+
+// firebase
+import { collection, getDocs } from "firebase/firestore";
+
+// context
+import { useAuth } from "@/contexts/AuthContext";
+import { useDB } from "@/contexts/DBContext";
+
+
+export default function Scan() {
+  const router = useRouter();
+  const { user, userEmail, logout } = useAuth()
+
+  // contexts
+  const db = useDB()
+
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      {/* header */}
+      <View style={styles.topBox}>
+        <Text style={styles.header}>Timber Guide</Text>
+        <Text style={styles.subText}>Answer a few questions to find the best timber for your needs</Text>
+      </View>
+
+      {/* scan */}
+      <View style={styles.scanContainer}>
+
+      </View>
+      
+    </SafeAreaView>
+  )
+};
+
+
+// styles
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: '#000',
+    },
+    topBox: {
+        marginHorizontal: 16,
+        marginTop: 16,
+        marginBottom: 5,
+        padding: 16,
+        backgroundColor: '#32003F',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 8,
+        color: '#fff',
+    },
+    subText: {
+        fontSize: 14,
+        textAlign: 'left',
+        color: '#ccc',
+    },
+    scanContainer: {
+        flex: 1,
+        padding: 10,
+        marginHorizontal: 16,
+        marginBottom: 25,
+        borderColor: "#fff",
+        borderWidth: 1,
+        borderRadius: 10,
+    },
+});
