bcf5886544d5e71b6a4f2d21c7d0ccdede3f8e51
CodedBeats <luca.haar@icloud.com>
2025-02-18 03:20:37 +1100

    fixed tabs focused style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Platform, View, TouchableOpacity, StyleSheet, Text, GestureResponderEvent } from 'react-native';
+import { useIsFocused } from '@react-navigation/native'; // couldn't figure it out with useRouter (there was so much pain to had here -_-)
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
@@ -8,7 +9,7 @@ import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
 
   return (
     <Tabs
@@ -48,13 +49,11 @@ export default function TabLayout() {
         name="Scan"
         options={{
           title: 'Scan',
-          tabBarButton: ({ onPress }) => (
-            <TouchableOpacity onPress={onPress} activeOpacity={0.7} style={styles.scanButton}>
-              <View style={styles.scanIconContainer}>
-                <IconSymbol size={30} name="camera.fill" color="white" />
-              </View>
-            </TouchableOpacity>
-          ),
+          tabBarButton: ({ onPress }) => {
+            const isFocused = useIsFocused(); 
+            console.log(isFocused)
+            return <CustomScanButton focused={isFocused} onPress={onPress} />
+          },
         }}
       />
       
@@ -63,7 +62,7 @@ export default function TabLayout() {
         name="TimberGuide"
         options={{
           title: 'Timber Guide',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="tree.fill" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="star.fill" color={color} />,
         }}
       />
       
@@ -80,6 +79,37 @@ export default function TabLayout() {
 }
 
 
+// custom scan nav btn with (needed for focus detection)
+// (i'll move this at some point....or fix it lol)
+const CustomScanButton = ({ focused, onPress }: { focused: boolean; onPress?: (event: GestureResponderEvent) => void }) => {
+  console.log(focused)
+
+  return (
+    <TouchableOpacity activeOpacity={0.7} style={styles.scanButton} onPress={onPress}>
+      <View style={[
+        styles.scanIconContainer,
+        // leaving this here  for style later
+        focused ? { backgroundColor: 'grey' } : { backgroundColor: 'grey' }
+      ]}>
+        <IconSymbol 
+          size={30} 
+          name="camera.fill" 
+          // leaving this here for style later
+          color={focused ? 'white' : 'white'} 
+        />
+      </View>
+      <Text style={[
+        styles.scanText,
+        focused ? { color: 'white' } : { color: '#7C7C7D' }
+      ]}>
+        Scan
+      </Text>
+    </TouchableOpacity>    
+  )
+}
+
+
+
 // Styles for the custom Scan button
 const styles = StyleSheet.create({
   scanButton: {
@@ -88,8 +118,14 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     alignItems: 'center',
-    alignSelf: 'center',
     justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  scanText: {
+    color: "#7C7C7D",
+    fontSize: 10,
+    fontFamily: "Segoe UI",
+    fontWeight: "bold",
   },
   scanIconContainer: {
     width: 50,
@@ -102,3 +138,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
