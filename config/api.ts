b3452a57cebdeef2264f2865707ba549280a7659
CodedBeats <luca.haar@icloud.com>
2025-04-20 13:59:11 +1000

    updated local ipv4 to be hidden in .env

--- config/api.ts
+++ config/api.ts
@@ -3,4 +3,4 @@ import { Platform } from 'react-native';
 export const BASE_URL =
   Platform.OS === 'web'
     ? 'http://localhost:3000'
-    : 'http://192.168.4.37:3000'; // Luca, replace this with your computer's local IP address
+    : process.env.EXPO_PUBLIC_LOCAL_IPV4;
