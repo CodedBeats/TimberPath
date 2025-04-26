9c136e609918d0024df7553fd71aa3b411c9b832
ariverapaniza <ariverapaniza@gmail.com>
2025-04-26 16:52:26 +1000

    Stable Version

--- config/api.ts
+++ config/api.ts
@@ -4,3 +4,4 @@ export const BASE_URL =
   Platform.OS === 'web'
     ? 'http://localhost:3000'
     : 'http://192.168.4.37:3000'; // Luca, replace this with your computer's local IP address
+    // stable version
